d40fb7cde61e16b509b14fa2c4c23e36ed371765
Hanna Kovalenko <annet-ko@i.ua>
2026-04-14 12:21:26 -0500

    TodoForm and TodoList files were added.App.jsx updated.

---
+++ src/TodoList.jsx
@@ -0,0 +1,20 @@
+
+function TodoList() {
+     const todoList = [
+    { id: 1, title: "Review resources" },
+    { id: 2, title: "Take notes" },
+    { id: 3, title: "Code out app" },
+    
+  ];
+  return (
+    <div>        
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
