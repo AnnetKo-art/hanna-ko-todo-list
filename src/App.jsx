dc8a6ea889a31fdd52897e604f9a60d1b01d2091
Hanna Kovalenko <annet-ko@i.ua>
2026-04-27 23:08:54 -0500

    onSubmit={handleAddTodo} was added to the form.

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,24 @@ import {useState} from 'react';
 import TodoList from "./TodoList.jsx";
 import TodoForm from "./TodoForm.jsx";
 
-
-const todos = [
-    { id: 1, title: "Review resources" },
-    { id: 2, title: "Take notes" },
-    { id: 3, title: "Code out app" },
-
-  ];
 function App() {
-  /*
-    KEY MOMENT: useState
-    - todoList = current state value
-    - setTodoList = function to update state
-    - todos = initial value
-  */
-  const[todoList, setTodoList]=useState(todos);
-  
+  const[todoList, setTodoList]=useState([]);
+  function addTodo(todoTitle){
+  let todo={
+  id:Date.now(),//to generate a unique id
+  title:todoTitle
+}
+    setTodoList(previous =>[todo, ...previous]);  
+  } 
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm/>
-      {/*
-        KEY MOMENT:
-        We pass state DOWN to child component via props
-        This is called "lifting state down"
-      */}   
-      <TodoList todoList={todoList} />    
+      <TodoForm onAddTodo={addTodo} />          
+      <TodoList todoList={todoList} /> 
     </div>
   );
 }
 
 export default App;
+
+
